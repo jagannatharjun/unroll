913561cfeee954d3711e9616b6c5f10b30ea7680
Prince Gupta <guptaprince8832@gmail.com>
2023-05-19 19:49:00 +0530

    use fusion style

--- gui/main.cpp
+++ gui/main.cpp
@@ -4,12 +4,15 @@
 #include "viewcontroller.hpp"
 #include "historycontroller.hpp"
 
+#include <QQuickStyle>
+
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
     QGuiApplication app(argc, argv);
+    QQuickStyle::setStyle("Fusion");
 
     qRegisterMetaType<QAbstractItemModel *>();
     qmlRegisterType<ViewController>("filebrowser", 0, 1, "ViewController");

f9bc143c306f4e298315148c40ddd1c760b1a1b1
Prince Gupta <guptaprince8832@gmail.com>
2023-07-09 16:27:20 +0530

    add build script

---
+++ build.bat
@@ -0,0 +1,27 @@
+SET SRC=E:\Projects\Gui\file-browser
+SET QTDIR=C:\Qt\6.5.1\msvc2019_64
+set LIBARCHIVE=C:\local\libarchive
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+echo ON
+
+mkdir build
+cd build
+
+cmake -G Ninja "-DCMAKE_PREFIX_PATH=%QTDIR%;%LIBARCHIVE%" %SRC%
+
+ninja 
+
+cd ..
+
+mkdir release
+cd release
+
+cp ..\build\gui\file-browser.exe file-browser.exe 
+
+%QTDIR%\bin\windeployqt.exe  --qmldir %SRC%\gui\qml file-browser.exe
+
+cp C:\local\libarchive\bin\archive.dll archive.dll
+
+cd ..

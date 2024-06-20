fec7a31b92855c3ef245d051e953516d38fa0fc9
Prince Gupta <guptaprince8832@gmail.com>
2024-06-20 13:42:33 +0530

    update build script

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 SET SRC=E:\Projects\Gui\file-browser
-SET QTDIR=C:\Qt\6.5.3\msvc2019_64
+SET QTDIR=C:\Qt\6.7.1\msvc2019_64
 set LIBARCHIVE=C:\local\libarchive
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
@@ -10,7 +10,7 @@ mkdir build
 cd build
 del CMakeCache.txt
 
-cmake -G Ninja "-DCMAKE_PREFIX_PATH=%QTDIR%;%LIBARCHIVE%" %SRC%
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release "-DCMAKE_PREFIX_PATH=%QTDIR%;%LIBARCHIVE%" %SRC%
 
 ninja 
 

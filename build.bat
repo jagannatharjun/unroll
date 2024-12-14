e3886c9daac7b13b4def814ac2267d1960c6fe3b
Prince Gupta <guptaprince8832@gmail.com>
2024-12-15 00:52:15 +0530

    update build info

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ SET SRC=E:\Projects\Gui\file-browser
 SET QTDIR=C:\Qt\6.7.3\msvc2019_64
 set LIBARCHIVE=C:\local\libarchive
 
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call  "E:\Libraries\VisualStudio\IDE\Community\VC\Auxiliary\Build\vcvars64.bat" x64
 
 echo ON
 

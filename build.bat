bb838e5b0ae595b70f6d42314f42285ee560e618
Prince Gupta <guptaprince8832@gmail.com>
2026-02-14 19:53:07 +0530

    improve build script

--- build.bat
+++ build.bat
@@ -1,31 +1,61 @@
-SET SRC=E:\Projects\Gui\file-browser
-SET QTDIR=C:\Qt\6.10.1\msvc2022_64
-set LIBARCHIVE=C:\local\libarchive
+@echo off
+SETLOCAL EnableDelayedExpansion
 
-call  "E:\Libraries\VisualStudio\IDE\Community\VC\Auxiliary\Build\vcvars64.bat" x64
+:: --- Configuration ---
+SET "SRC=E:\Projects\Gui\file-browser"
+SET "QTDIR=C:\Qt\6.10.1\msvc2022_64"
+SET "LIBARCHIVE=C:\local\libarchive"
+SET "VCVARS=E:\Libraries\VisualStudio\IDE\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-echo ON
+echo [1/6] Validating paths...
+if not exist "%SRC%" echo Error: Source path not found. && pause && exit /b 1
+if not exist "%VCVARS%" echo Error: Visual Studio vcvars not found. && pause && exit /b 1
 
-mkdir build
-cd build
-:: del CMakeCache.txt
+:: --- Environment Setup ---
+echo [2/6] Initializing MSVC Environment...
+call "%VCVARS%" x64
+if %ERRORLEVEL% neq 0 echo Failed to set up MSVC environment. && pause && exit /b 1
 
-cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo  -DDB_TEST=OFF "-DCMAKE_PREFIX_PATH=%QTDIR%;%LIBARCHIVE%" %SRC%
+:: --- Build Process ---
+if not exist build mkdir build
+cd build || exit /b 1
 
-ninja 
+echo [3/6] Running CMake...
+cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DDB_TEST=OFF "-DCMAKE_PREFIX_PATH=%QTDIR%;%LIBARCHIVE%" "%SRC%"
+if %ERRORLEVEL% neq 0 echo CMake configuration failed. && pause && exit /b 1
+
+echo [4/6] Running Ninja Build...
+ninja
+if %ERRORLEVEL% neq 0 echo Build failed. && pause && exit /b 1
 
 cd ..
 
-mkdir release
-cd release
+:: --- Deployment ---
+echo [5/6] Preparing Release Folder...
+if not exist release mkdir release
+cd release || exit /b 1
 
-copy ..\build\gui\file-browser.exe file-browser.exe 
-copy ..\build\gui\file-browser.pdb file-browser.pdb 
+copy /Y ..\build\gui\file-browser.exe file-browser.exe || goto :error
+copy /Y ..\build\gui\file-browser.pdb file-browser.pdb || goto :error
 
-%QTDIR%\bin\windeployqt.exe  --qmldir %SRC%\gui\qml file-browser.exe
+echo [6/6] Running windeployqt...
+"%QTDIR%\bin\windeployqt.exe" --qmldir "%SRC%\gui\qml" file-browser.exe
+if %ERRORLEVEL% neq 0 echo Deployment tool failed. && pause && exit /b 1
 
-copy %LIBARCHIVE%\bin\archive.dll archive.dll
+copy /Y "%LIBARCHIVE%\bin\archive.dll" archive.dll || goto :error
 
+echo.
+echo ===========================================
+echo Build and Deployment Successful!
+echo ===========================================
 cd ..
+pause
+exit /b 0
 
-PAUSE
+:error
+echo.
+echo !------- ERROR DETECTED -------!
+echo An error occurred during the file copying process.
+echo !------------------------------!
+pause
+exit /b 1
